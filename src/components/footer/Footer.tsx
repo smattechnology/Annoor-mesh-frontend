c3178d4fcf6eb1a6cd5e06477bc3fd30c892bb8f
smattechnology <abirhasan.devloper@gmail.com>
2025-07-31 16:14:57 +0600

    whatsapp number and avatar update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React from "react";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-
 const Footer = () => {
   return (
     <div className="footer flex flex-col items-center justify-center p-8 border-t border-gray-200 bg-3 text-gray-300">
       <p>© 2025 Mess Bazar - Annoor Foods. All rights reserved.</p>
       <FloatingWhatsApp
-        phoneNumber="+8801840031889"
-        accountName="Tech Support"
+        phoneNumber="+8801303739522"
+        accountName="Mess Bazar - Annoor Foods"
         notification
         notificationSound
+        className="text-black"
+        avatar="/img/headset.jpg"
         onSubmit={(e) => {
           console.log(e);
         }}
