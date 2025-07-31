06ffda5d397e4cee1d2ba7b3e220ca784d493ce0
smattechnology <abirhasan.devloper@gmail.com>
2025-07-31 14:22:36 +0600

    note added commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import { FoodSelectionPage } from "../components/FoodSelectionPage";
 
 export default function Page() {
-  return <FoodSelectionPage />;
+  return (
+    <div className="">
+      <FoodSelectionPage />
+      <div className="mt-4"></div>
+      <label htmlFor="note" className="block text-sm font-medium text-gray-700">
+        Note
+      </label>
+      <input
+        type="text"
+        id="note"
+        name="note"
+        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        placeholder="Add a note..."
+      />
+    </div>
+  );
 }
