8b12f25f06af88e4ea12cb55db8f641e49ffc9c9
smattechnology <abirhasan.devloper@gmail.com>
2025-08-02 23:58:28 +0600

    fix-meal route delete

--- src/app/fix-meal/page.tsx
+++
@@ -1,6 +0,0 @@
-import { FoodSelectionPage } from "../../components/FoodSelectionPage";
-import { ItemCard } from "../..//components/ItemCard";
-import { useMealSelections } from "../..//hooks/useMealSelections";
-export default function FixMealPage() {
-  return <FoodSelectionPage />;
-}
